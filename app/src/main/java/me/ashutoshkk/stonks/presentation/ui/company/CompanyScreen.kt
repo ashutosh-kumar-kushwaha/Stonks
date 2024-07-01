b6d408b8988ab37f45a3bb71d7f8a7cfe2300967
Ashutosh <akkushwaha0104@gmail.com>
2024-07-01 15:34:04 +0530

    Improve Graph Selector UI

--- app/src/main/java/me/ashutoshkk/stonks/presentation/ui/company/CompanyScreen.kt
+++ app/src/main/java/me/ashutoshkk/stonks/presentation/ui/company/CompanyScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -38,7 +38,7 @@ fun CompanyScreen() {
     var selectedGraph by remember {
         mutableStateOf(GraphType.Day)
     }
-    val graphData = when(selectedGraph){
+    val graphData = when (selectedGraph) {
         GraphType.Day -> graphUiState.day
         GraphType.Week -> graphUiState.week
         GraphType.Month -> graphUiState.month
@@ -82,15 +82,18 @@ fun CompanyScreen() {
                         )
                     }
                 }
-                if(graphData == null) {
+                if (graphData == null) {
                     viewModel.fetchGraphData(selectedGraph)
-                } else{
+                } else {
                     LineChart(
                         labels = graphData.labels,
                         modelProducer = graphData.modelProducer
                     )
                 }
-                GraphSelector(selectedGraph = selectedGraph){
+                GraphSelector(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    selectedGraph = selectedGraph
+                ) {
                     selectedGraph = it
                 }
                 Text(
--- app/src/main/java/me/ashutoshkk/stonks/presentation/ui/company/components/GraphSelector.kt
+++ app/src/main/java/me/ashutoshkk/stonks/presentation/ui/company/components/GraphSelector.kt
@@ -1,9 +1,11 @@
 package me.ashutoshkk.stonks.presentation.ui.company.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -13,20 +15,31 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import me.ashutoshkk.stonks.presentation.ui.company.GraphType
 import me.ashutoshkk.stonks.presentation.ui.theme.StonksTheme
 
 @Composable
-fun GraphSelector(selectedGraph: GraphType, onGraphSelect: (GraphType) -> Unit) {
+fun GraphSelector(
+    modifier: Modifier,
+    selectedGraph: GraphType,
+    onGraphSelect: (GraphType) -> Unit
+) {
     Card(
-        modifier = Modifier
-            .height(64.dp)
-            .padding(StonksTheme.paddings.allMedium)
-            .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(64.dp))
+        modifier = modifier
+            .height(64.dp),
+        shape = RoundedCornerShape(64.dp),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
     ) {
-        Row {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(StonksTheme.paddings.around)
+        ) {
             GraphType.entries.forEach {
                 GraphTypeItem(selectedGraph = selectedGraph, graphType = it) {
                     onGraphSelect(it)
@@ -38,31 +51,34 @@ fun GraphSelector(selectedGraph: GraphType, onGraphSelect: (GraphType) -> Unit) 
 
 @Composable
 fun GraphTypeItem(selectedGraph: GraphType, graphType: GraphType, onGraphSelect: () -> Unit) {
-    Text(
-        text = when (graphType) {
-            GraphType.Day -> "1D"
-            GraphType.Week -> "1W"
-            GraphType.Month -> "1M"
-            GraphType.SixMonth -> "6M"
-            GraphType.Year -> "1Y"
-        },
+    Box(
         modifier = if (graphType == selectedGraph) {
             Modifier
-//                .padding(StonksTheme.paddings.around)
-                .size(32.dp)
+                .size(48.dp)
                 .background(MaterialTheme.colorScheme.primary, CircleShape)
                 .clickable {
                     onGraphSelect()
                 }
         } else {
             Modifier
-                .size(32.dp)
+                .size(48.dp)
                 .clickable {
                     onGraphSelect()
                 }
-//                .padding(StonksTheme.paddings.around)
         },
-        style = StonksTheme.typography.headlineLarge,
-        color = StonksTheme.colorScheme.text
-    )
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = when (graphType) {
+                GraphType.Day -> "1D"
+                GraphType.Week -> "1W"
+                GraphType.Month -> "1M"
+                GraphType.SixMonth -> "6M"
+                GraphType.Year -> "1Y"
+            },
+            textAlign = TextAlign.Center,
+            style = StonksTheme.typography.headlineSmall,
+            color = StonksTheme.colorScheme.text
+        )
+    }
 }
