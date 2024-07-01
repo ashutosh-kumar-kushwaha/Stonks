9ab083babc94699a98add4675a28aaeafd8aaf20
Ashutosh <akkushwaha0104@gmail.com>
2024-07-01 10:30:53 +0530

    Add Meta Data in Serializer for Graph API

--- app/src/main/java/me/ashutoshkk/stonks/data/remote/deserializer/GraphPointsDeserializer.kt
+++ app/src/main/java/me/ashutoshkk/stonks/data/remote/deserializer/GraphPointsDeserializer.kt
@@ -20,22 +20,22 @@ class GraphPointsDeserializer : JsonDeserializer<GraphDataDto> {
         }
         val obj = json.asJsonObject
         val key = obj.keySet().last()
-        val graphPointsSet = obj.get(key).asJsonObject.entrySet()
+        val graphPointsSet = obj[key].asJsonObject.entrySet()
         val graphPoints = graphPointsSet.map {
             val time = it.key
-            val open = it.value.asJsonObject.get("1. open").asDouble
-            val high = it.value.asJsonObject.get("2. high").asDouble
-            val low = it.value.asJsonObject.get("3. low").asDouble
-            val close = it.value.asJsonObject.get("4. close").asDouble
-            val volume = it.value.asJsonObject.get("5. volume").asLong
+            val open = it.value.asJsonObject["1. open"].asDouble
+            val high = it.value.asJsonObject["2. high"].asDouble
+            val low = it.value.asJsonObject["3. low"].asDouble
+            val close = it.value.asJsonObject["4. close"].asDouble
+            val volume = it.value.asJsonObject["5. volume"].asLong
             GraphPointsDto(time, PriceDto(open, high, low, close, volume))
         }
-        val meta = obj.get("Meta Data").asJsonObject
-        val information = meta.get("1. Information").asString
-        val symbol = meta.get("2. Symbol").asString
-        val lastRefreshed = meta.get("3. Last Refreshed").asString
-        val outputSize = meta.get("4. Output Size").asString
-        val timeZone = meta.get("5. Time Zone").asString
+        val meta = obj["Meta Data"].asJsonObject
+        val information = meta["1. Information"].asString
+        val symbol = meta["2. Symbol"].asString
+        val lastRefreshed = meta["3. Last Refreshed"].asString
+        val outputSize = meta["4. Output Size"].asString
+        val timeZone = meta["5. Time Zone"].asString
         val metaData = GraphMetaDataDto(information, symbol, lastRefreshed, outputSize, timeZone)
 
         return GraphDataDto(metaData, graphPoints)
